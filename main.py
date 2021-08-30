aa128613d1d58c9987712e961b595e54af45590c
Yanosik2115 <lukwaszczak@gmail.com>
2021-08-31 00:31:13 +0200

    CPU & Kernel scan

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os 
 import sys
 import platform
+import psutil
 
 time = os.popen('uptime -p').read()[:-1]
-arch = platform.architecture()
-mach = platform.machine()
+
+uname = platform.uname()
+
+cpucount = psutil.cpu_count(logical=True)
+
+str(cpucount)
+
+file1 = open('/proc/version', 'r')
+
+proc =  file1.read().split()
+
+file2 = open('/proc/cpuinfo', 'r')
+
+cpu = ' '.join(file2.readlines()[4].split()[3:])
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,13 +31,16 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-print(f'{bcolors.WARNING}.....................' + f'{bcolors.FAIL}/´¯¯/' + f'{bcolors.OKBLUE}          Current Directory: ' + f'{bcolors.HEADER}' + os.getcwd())
-print(f'{bcolors.WARNING}...................' + f'{bcolors.FAIL},/¯.../' + f'{bcolors.OKBLUE}          Uptime: ' + f'{bcolors.HEADER}' + time) 
-print(f"{bcolors.WARNING}..................." + f"{bcolors.FAIL}/..../'" + f'{bcolors.OKBLUE}          Platform: ' + f'{bcolors.HEADER}' + sys.platform + ' ' + platform.architecture()[0] + ' ' + platform.machine())
-print("............../´¯/'..'/´¯¯`·¸")
-print(".........../'/.../..../....../¨¯")
-print("..........('(....´...´... ¯~/'..')")
-print("...........\..............'...../")
-print("............\....\.........._.·´")
-print(".............\..............(")
-print('..............\..............(')
+print(' '*20 + f'{bcolors.FAIL}/´¯¯/' + f'{bcolors.OKBLUE}           Current Directory: ' + f'{bcolors.HEADER}' + os.getcwd())
+print(' '*19 + f'{bcolors.FAIL}/¯.../' + f'{bcolors.OKBLUE}           Uptime: ' + f'{bcolors.HEADER}' + time) 
+print(' '*18 + f"{bcolors.FAIL}/..../'" + f'{bcolors.OKBLUE}           Platform: ' + f'{bcolors.HEADER}' + proc[0] + ' ' + platform.architecture()[0] + ' ' + platform.machine())
+print(' '*13 + f"{bcolors.FAIL}/´¯/'..'/´¯¯`·¸" + f'{bcolors.OKBLUE}        Machine: ' + f'{bcolors.HEADER}' + proc[9] + ' ' + f'{bcolors.OKBLUE} Cores: ' + f'{bcolors.HEADER}' + ' ' + f'{str(cpucount)}')
+print(' '*10 + f"{bcolors.FAIL}/'/.../..../....../¨')" + f'{bcolors.OKBLUE}    Kernel: ' + f'{bcolors.HEADER}' + proc[2])
+print(' '*9 +  f"{bcolors.FAIL}('(....´...´... ¯~/'..')" + f'{bcolors.OKBLUE}   CPU: ' + f'{bcolors.HEADER}' + cpu)
+print(' '*10 + f"{bcolors.FAIL}\..............'...../")
+print(' '*11 + f"{bcolors.FAIL}\....\.........._.·´")
+print(' '*12 + f"{bcolors.FAIL}\..............(")
+print(' '*13 + f'{bcolors.FAIL}\..............(')
+
+file1.close()
+file2.close()
--- test.py
+++ test.py
@@ -1,4 +1,19 @@
+from os import close
 import platform
+import sys 
 
-print(platform.architecture()[0])
-print(platform.machine())
+file1 = open('/proc/cpuinfo', 'r')
+
+cpu = ' '.join(file1.readlines()[4].split()[3:])
+
+print(cpu)
+
+file1.close()
+
+
+# uname = platform.uname()
+
+
+# print(uname.version.split()[0])
+#print(platform.architecture()[0])
+#print(platform.machine())
