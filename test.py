aea65cf0ff5a436e5bab3ceb3dd4c7d516aac26a
259625 <259625@student.pwr.edu.pl>
2021-09-13 14:22:29 +0200

    #

--- test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def getOutput(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True,stderr=subprocess.PIPE).communicate()
-
-    print(p[0].decode('utf-8'))
-
-getOutput('cat /proc/meminfo')
